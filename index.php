d71fcd06ca34878f31177d3e69a518a6ebe67d9a
MatteDev02 <62772891+MattDEV02@users.noreply.github.com>
2021-02-07 16:05:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 
 $key = 'wifi';
 $cond = ((!empty($_POST)) && (isset($_POST[$key])));
-$data = $cond ? $_POST[$key] : null;
-$arr = json_decode($data);
-$str = json_encode($arr,JSON_PRETTY_PRINT);
-$json = ('<pre>'.$str.'</pre>');
-$file = fopen('wifi.html', 'w') or die('Unable to open file!');
-fwrite($file, $json) or die('Unable to write file!');
-fclose($file) or die('Unable to close file!');
+if($cond) {
+   $data = $_POST[$key];
+   $arr = json_decode($data);
+   $str = json_encode($arr,JSON_PRETTY_PRINT);
+   $json = ('<pre>'.$str.'</pre>');
+   $file = fopen('wifi.html', 'w') or die('Unable to open file!');
+   fwrite($file, $json) or die('Unable to write file!');
+   fclose($file) or die('Unable to close file!');
+} else echo 'No WLAN checked.';
 
 ?>
